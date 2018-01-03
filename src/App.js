60ab0a66d3dbe8591a69ee8b42004af0567c0501
Jason Nuhn <jason.a.nuhn@gmail.com>
2018-01-03 09:48:48 -0500

    added files to allow for yarn start

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+
+import Footer from './Footer/Footer';
+import Home from './Home/Home';
+//import QuestionHome from './QuestionHome';
+import Language from './Language/Language';
+import UsersList from './Users/Users';
+import SearchBar from './Search/Search';
+import Login from './Login/Login';
+import Signup from './Signup/Signup';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App-header">
+            <ul>
+              <Link to="/" className="active">Home</Link>
+              {/* <Link to="/Question" className="active">Question</Link> */}
+              <Link to="/Language" className="active">Language</Link>
+              <Link to="/Userslist" className="active">Users</Link>
+              <Link to="/SearchBar" className="active">Search</Link>
+              <Link to="/Login" className="active">Log In</Link>
+              <Link to="/Signup" className="active">Sign Up</Link>
+            </ul>
+            
+            
+            
+            <Route exact path="/" component={Home} />
+            {/* <Route path="/Question" component={QuestionHome} /> */}
+            <Route path="/Language" component={Language} />
+            <Route path="/Userslist" component={UsersList} />
+            <Route path="/SearchBar" component={SearchBar} />
+            <Route path="/Login" component={Login} />
+            <Route path="/Signup" component={Signup} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
